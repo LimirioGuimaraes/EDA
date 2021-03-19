b7677e282d2bda3867a72fc893e7de1f2d9c796f
Matheus Pimentel <matheusp.eng@gmail.com>
2021-03-18 22:04:33 -0300

    Melhoria no código

--- DEFINITIVO/Bibliotecas/CriarBOW.h
+++ DEFINITIVO/Bibliotecas/CriarBOW.h
@@ -2,18 +2,18 @@
 #include <stdlib.h>
 #include <string.h>
 
- FILE *CriarBOW(FILE *bow, FILE *trA, FILE *dicionario, char nomeArquivo[10], int *cont){
+ FILE *CriarBOW(FILE *bow, FILE *tr, FILE *dicionario, char nomeArquivo[10], int *cont){
      char ch[50], palavra[50];
-     int i, j = 0, k;
+     int i, j = 0;
      //Abertura do arquivo
      bow = fopen(nomeArquivo, "w+");
      //Cópia do arquivo dicionário em BOW
      while(fscanf(dicionario, "%s", ch) != EOF){
-         //voltar trA para o início do arquivo em todo incício de laço
-         rewind(trA);
+         //voltar tr para o início do arquivo em todo incício de laço
+         rewind(tr);
          //Zerar o i em todo inicio de laço
          i = 0;
-         while(fscanf(trA, "%s", palavra) != EOF){
+         while(fscanf(tr, "%s", palavra) != EOF){
 
              if (strcmp(palavra,ch) == 0){
                  i++;
@@ -27,13 +27,13 @@
          j++;
      }
 
-     //Lógica para comparação de dicionário em TRA
+     //Lógica para comparação de dicionário em tr
 
      //checagem do ponteiro e mensagem de confirmação
      if (bow == NULL){
          printf("\nFalha na criação ou no acesso ao arquivo...\n");
      } else {
-         
+
          printf("\nO arquivo %s foi criado!\n\n", nomeArquivo);
      }
 
