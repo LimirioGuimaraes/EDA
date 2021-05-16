a096b641331b2222a2d24f94a4532f685b83e15c
Matheus Pimentel Leal <62526025+Matheuspleal@users.noreply.github.com>
2021-05-16 14:12:23 -0300

    Add files via upload

--- CalcFatBal.c
+++ CalcFatBal.c
@@ -2,17 +2,34 @@
 #include <stdlib.h>
 #include "GeraABP.c"
 
+int CalcAltura(No *no){
+    int tamDir = 0, tamEsq = 0;
 
+    if (no == NULL){
+        return 0;
+    } else {
+        if (no -> pEsq != NULL){
+            tamEsq = CalcAltura(no -> pEsq) + 1;
+        }
+        if (no -> pDir != NULL){
+            tamDir = CalcAltura(no -> pDir) + 1;
+        }
+    }
+
+    if (tamEsq <= tamDir){
+        return tamEsq;
+    } else {
+        return tamDir;
+    }
+}
 
 No *CalcFatBal(No *cabeca){
+    
     // Se o nó cabeça for nulo, retornar Fator de balanceamento 0
     if(cabeca == NULL){
         cabeca -> FatBal = 0;
         return cabeca;
     }
-    No *aux = cabeca;
-
-    aux -> FatBal = EncontraFatBal(aux);
 
     return cabeca;
 }
